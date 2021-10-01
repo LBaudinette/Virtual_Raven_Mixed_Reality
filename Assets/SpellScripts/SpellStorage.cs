fbaaf3a286b1281de14b56d16090f7cd124917f7
rmit-s3722631-Daniel-Snowsill <danielsnowsill@live.com.au>
2021-10-01 18:39:15 +1000

    Started spell shield

---
+++ Assets/SpellScripts/SpellStorage.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class SpellStorage : MonoBehaviour
+{
+    public Transform spellOneTransform;
+    public Transform spellTwoTransform;
+
+    private bool spellOneEmpty;
+    private bool spellTwoEmpty;
+
+    public GameObject spellSlots;
+    public GameObject testSpell;
+
+    public GameObject earthSpell;
+    public GameObject fireSpell;
+    public GameObject iceSpell;
+
+    public enum SpellType {
+        fire, ice, earth
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addSpell(SpellType newSpell) {
+        if (spellOneEmpty) {
+            createSpell(newSpell, spellOneTransform);
+        }
+        else if (spellTwoEmpty) {
+            createSpell(newSpell, spellTwoTransform);
+        }
+        //If both slots are full then don't add this new spell
+    }
+
+    private void createSpell(SpellType newSpell, Transform positionTransform) {
+        switch (newSpell) {
+            case SpellType.earth:
+                Instantiate(earthSpell, positionTransform);
+                break;
+            case SpellType.ice:
+                Instantiate(iceSpell, positionTransform);
+                break;
+            case SpellType.fire:
+                Instantiate(fireSpell, positionTransform);
+                break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log($"COLLISION NAME: {other.gameObject.name}");
+        switch (other.gameObject.tag) {
+            case "IceWizardSpell":
+                addSpell(SpellType.ice);
+                break;
+            case "EarthWizardSpell":
+                addSpell(SpellType.earth);
+                break;
+            case "FireWizardSpell":
+                addSpell(SpellType.fire);
+                break;
+        }
+        
+        //Destroy the spell that collided with it
+        Destroy(other.gameObject);
+    }
+}
